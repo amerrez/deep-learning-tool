038cc9e0add303caffb6dbb0d110bedf751d14ed
amerrez <arez@hawk.iit.edu>
2018-04-22 13:58:31 -0700

    seperating flask from keras

---
+++ keras-advanced-rest-api/flask_server.py
@@ -0,0 +1,136 @@
+import flask
+from flask import render_template, flash, redirect
+
+# initialize our Flask application, Redis server, and Keras model
+from config import Config
+from forms import LoginForm
+from forms import UploadForm
+app = flask.Flask(__name__)
+app.config.from_object(Config)
+
+#db redis
+import redis
+db = redis.StrictRedis(host="localhost", port=6379, db=0)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template('index.html', title='Amer')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/data_upload')
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/data_upload', methods=['GET', 'POST'])
+def upload():
+	form = UploadForm()
+	if form.validate_on_submit():
+# 		filename = secure_filename(form.file.data.filename)
+# 		form.file.data.save('uploads/' + filename)
+		return redirect('/index')
+	return render_template('data_upload.html', title='Upload', form=form)
+
+@app.route('/train')	
+def simple_train():
+	# USAGE
+	# python simple_request.py
+
+	# import the necessary packages
+	import requests
+
+	# initialize the Keras REST API endpoint URL along with the input
+	# image path
+	KERAS_REST_API_URL = "http://localhost:5000/predict"
+# 	return '''<p> link http://localhost:5000/predict loaded</p>'''
+	IMAGE_PATH = "jemma.png"
+# 	return '''<p> image path loaded</p>'''
+	# load the input image and construct the payload for the request
+	image = open(IMAGE_PATH, "rb").read()
+	payload = {"image": image}
+# 	return '''<p> image opened and loaded</p>'''
+	# submit the request
+	r = requests.post(KERAS_REST_API_URL, files=payload).json()
+	
+	return '''<p> request Json returned to variable r</p>'''
+	# ensure the request was sucessful
+	if r["success"]:
+		return '''<p> request suceeded</p>'''
+
+		# loop over the predictions and display them
+		return render_tempalte('index.html', task='show_results', data=r["predictions"])
+# 		for (i, result) in enumerate(r["predictions"]):
+# 			print("{}. {}: {:.4f}".format(i + 1, result["label"],
+# 				result["probability"]))
+
+	# otherwise, the request failed
+	else:
+		return '''<p> request failed</p>'''	
+		print("Request failed")	
+	
+@app.route("/predict", methods=["POST"])
+def predict():
+	# initialize the data dictionary that will be returned from the
+	# view
+	data = {"success": False}
+
+	# ensure an image was properly uploaded to our endpoint
+	if flask.request.method == "POST":
+		if flask.request.files.get("image"):
+			# read the image in PIL format and prepare it for
+			# classification
+			image = flask.request.files["image"].read()
+			image = Image.open(io.BytesIO(image))
+			image = prepare_image(image, (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+			# ensure our NumPy array is C-contiguous as well,
+			# otherwise we won't be able to serialize it
+			image = image.copy(order="C")
+
+			# generate an ID for the classification then add the
+			# classification ID + image to the queue
+			k = str(uuid.uuid4())
+			d = {"id": k, "image": base64_encode_image(image)}
+			db.rpush(IMAGE_QUEUE, json.dumps(d))
+
+			# keep looping until our model server returns the output
+			# predictions
+			while True:
+				# attempt to grab the output predictions
+				output = db.get(k)
+
+				# check to see if our model has classified the input
+				# image
+				if output is not None:
+ 					# add the output predictions to our data
+ 					# dictionary so we can return it to the client
+					output = output.decode("utf-8")
+					data["predictions"] = json.loads(output)
+
+					# delete the result from the database and break
+					# from the polling loop
+					db.delete(k)
+					break
+
+				# sleep for a small amount to give the model a chance
+				# to classify the input image
+				time.sleep(CLIENT_SLEEP)
+
+			# indicate that the request was a success
+			data["success"] = True
+
+	# return the data dictionary as a JSON response
+	return flask.jsonify(data)
+
+# if this is the main thread of execution
+# then start the server
+if __name__ == "__main__":
+
+	# start the web server
+	print("* Starting web service...")
+	app.run()
--- keras-advanced-rest-api/run_keras_server.py
+++ keras-advanced-rest-api/run_keras_server.py
@@ -14,7 +14,6 @@ from threading import Thread
 from PIL import Image
 import numpy as np
 import base64
-import flask
 import redis
 import uuid
 import time
@@ -23,7 +22,6 @@ import sys
 import io
 
 
-from flask import render_template, flash, redirect
 
 
 # initialize constants used to control image spatial dimensions and
@@ -38,16 +36,6 @@ IMAGE_QUEUE = "image_queue"
 BATCH_SIZE = 32
 SERVER_SLEEP = 0.25
 CLIENT_SLEEP = 0.25
-
-# initialize our Flask application, Redis server, and Keras model
-from config import Config
-from forms import LoginForm
-from forms import UploadForm
-app = flask.Flask(__name__)
-app.config.from_object(Config)
-# app.config['SECRET_KEY'] = 'you-will-never-guess'
-
-import routes
 
 db = redis.StrictRedis(host="localhost", port=6379, db=0)
 model = None
@@ -148,130 +136,10 @@ def classify_process():
 		time.sleep(SERVER_SLEEP)
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-	return render_template('index.html', title='Amer')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/data_upload')
-    return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/data_upload', methods=['GET', 'POST'])
-def upload():
-	form = UploadForm()
-	if form.validate_on_submit():
-# 		filename = secure_filename(form.file.data.filename)
-# 		form.file.data.save('uploads/' + filename)
-		return redirect('/index')
-	return render_template('data_upload.html', title='Upload', form=form)
-
-@app.route('/train')	
-def simple_train():
-	# USAGE
-	# python simple_request.py
-
-	# import the necessary packages
-	import requests
-
-	# initialize the Keras REST API endpoint URL along with the input
-	# image path
-	KERAS_REST_API_URL = "http://localhost:5000/predict"
-# 	return '''<p> link http://localhost:5000/predict loaded</p>'''
-	IMAGE_PATH = "jemma.png"
-# 	return '''<p> image path loaded</p>'''
-	# load the input image and construct the payload for the request
-	image = open(IMAGE_PATH, "rb").read()
-	payload = {"image": image}
-# 	return '''<p> image opened and loaded</p>'''
-	# submit the request
-	r = requests.post(KERAS_REST_API_URL, files=payload).json()
-	
-	return '''<p> request Json returned to variable r</p>'''
-	# ensure the request was sucessful
-	if r["success"]:
-		return '''<p> request suceeded</p>'''
-
-		# loop over the predictions and display them
-		return render_tempalte('index.html', task='show_results', data=r["predictions"])
-# 		for (i, result) in enumerate(r["predictions"]):
-# 			print("{}. {}: {:.4f}".format(i + 1, result["label"],
-# 				result["probability"]))
-
-	# otherwise, the request failed
-	else:
-		return '''<p> request failed</p>'''	
-		print("Request failed")	
-	
-@app.route("/predict", methods=["POST"])
-def predict():
-	# initialize the data dictionary that will be returned from the
-	# view
-	data = {"success": False}
-
-	# ensure an image was properly uploaded to our endpoint
-	if flask.request.method == "POST":
-		if flask.request.files.get("image"):
-			# read the image in PIL format and prepare it for
-			# classification
-			image = flask.request.files["image"].read()
-			image = Image.open(io.BytesIO(image))
-			image = prepare_image(image, (IMAGE_WIDTH, IMAGE_HEIGHT))
-
-			# ensure our NumPy array is C-contiguous as well,
-			# otherwise we won't be able to serialize it
-			image = image.copy(order="C")
-
-			# generate an ID for the classification then add the
-			# classification ID + image to the queue
-			k = str(uuid.uuid4())
-			d = {"id": k, "image": base64_encode_image(image)}
-			db.rpush(IMAGE_QUEUE, json.dumps(d))
-
-			# keep looping until our model server returns the output
-			# predictions
-			while True:
-				# attempt to grab the output predictions
-				output = db.get(k)
-
-				# check to see if our model has classified the input
-				# image
-				if output is not None:
- 					# add the output predictions to our data
- 					# dictionary so we can return it to the client
-					output = output.decode("utf-8")
-					data["predictions"] = json.loads(output)
-
-					# delete the result from the database and break
-					# from the polling loop
-					db.delete(k)
-					break
-
-				# sleep for a small amount to give the model a chance
-				# to classify the input image
-				time.sleep(CLIENT_SLEEP)
-
-			# indicate that the request was a success
-			data["success"] = True
-
-	# return the data dictionary as a JSON response
-	return flask.jsonify(data)
-
 # if this is the main thread of execution first load the model and
 # then start the server
 if __name__ == "__main__":
 	# load the function used to classify input images in a *separate*
 	# thread than the one used for main classification
 	print("* Starting model service...")
-	t = Thread(target=classify_process, args=())
-	t.daemon = True
-	t.start()
-
-	# start the web server
-	print("* Starting web service...")
-	app.run()
+	classify_process()
