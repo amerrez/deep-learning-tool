979979a8f722dcd75c29976ff01deedb2af4fe0e
thuansjsu <thuan.bao@sjsu.edu>
2018-04-25 22:17:45 -0700

    Refactor and integrate bootstrap + jquery

---
+++ keras/run_keras.py
@@ -0,0 +1,122 @@
+# USAGE
+# Start the server:
+# 	python run_keras_server.py
+# Submit a request via cURL:
+# 	curl -X POST -F image=@jemma.png 'http://localhost:5000/predict'
+# Submita a request via Python:
+#	python simple_request.py 
+
+import base64
+import json
+import sys
+import time
+
+import numpy as np
+import redis
+# import the necessary packages
+
+# initialize constants used to control image spatial dimensions and
+# data type
+from keras.applications import ResNet50, imagenet_utils
+
+IMAGE_WIDTH = 224
+IMAGE_HEIGHT = 224
+IMAGE_CHANS = 3
+IMAGE_DTYPE = "float32"
+
+# initialize constants used for server queuing
+IMAGE_QUEUE = "image_queue"
+BATCH_SIZE = 32
+SERVER_SLEEP = 0.25
+CLIENT_SLEEP = 0.25
+
+db = redis.StrictRedis(host="localhost", port=6379, db=0)
+model = None
+
+
+def base64_decode_image(a, dtype, shape):
+    # if this is Python 3, we need the extra step of encoding the
+    # serialized NumPy string as a byte object
+    if sys.version_info.major == 3:
+        a = bytes(a, encoding="utf-8")
+
+    # convert the string to a NumPy array using the supplied data
+    # type and target shape
+    a = np.frombuffer(base64.decodestring(a), dtype=dtype)
+    a = a.reshape(shape)
+
+    # return the decoded image
+    return a
+
+
+def classify_process():
+    # load the pre-trained Keras model (here we are using a model
+    # pre-trained on ImageNet and provided by Keras, but you can
+    # substitute in your own networks just as easily)
+    print("* Loading model...")
+    model = ResNet50(weights="imagenet")
+    print("* Model loaded")
+
+    # continually pool for new images to classify
+    while True:
+        # attempt to grab a batch of images from the database, then
+        # initialize the image IDs and batch of images themselves
+        queue = db.lrange(IMAGE_QUEUE, 0, BATCH_SIZE - 1)
+        imageIDs = []
+        batch = None
+
+        # loop over the queue
+        for q in queue:
+            # deserialize the object and obtain the input image
+            q = json.loads(q.decode("utf-8"))
+            image = base64_decode_image(q["image"], IMAGE_DTYPE,
+                                        (1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANS))
+
+            # check to see if the batch list is None
+            if batch is None:
+                batch = image
+
+            # otherwise, stack the data
+            else:
+                batch = np.vstack([batch, image])
+
+            # update the list of image IDs
+            imageIDs.append(q["id"])
+
+        # check to see if we need to process the batch
+        if len(imageIDs) > 0:
+            # classify the batch
+            print("* Batch size: {}".format(batch.shape))
+            preds = model.predict(batch)
+            results = imagenet_utils.decode_predictions(preds)
+
+            # loop over the image IDs and their corresponding set of
+            # results from our model
+            for (imageID, resultSet) in zip(imageIDs, results):
+                # initialize the list of output predictions
+                output = []
+
+                # loop over the results and add them to the list of
+                # output predictions
+                for (imagenetID, label, prob) in resultSet:
+                    r = {"label": label, "probability": float(prob)}
+                    output.append(r)
+
+                # store the output predictions in the database, using
+                # the image ID as the key so we can fetch the results
+                db.set(imageID, json.dumps(output))
+
+            # remove the set of images from our queue
+            db.ltrim(IMAGE_QUEUE, len(imageIDs), -1)
+
+        # sleep for a small amount
+        time.sleep(SERVER_SLEEP)
+
+
+# if this is the main thread of execution first load the model and
+# then start the server
+if __name__ == "__main__":
+    # load the function used to classify input images in a *separate*
+    # thread than the one used for main classification
+    print("* Starting model service...")
+    classify_process()
